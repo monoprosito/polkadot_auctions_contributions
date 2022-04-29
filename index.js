2163dee849d3ea8dacea71161d3b9f611895c17b
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-04-28 23:10:48 -0500

    Adds support to Subscan

--- index.js
+++ index.js
@@ -1,73 +1,138 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+// import { ApiPromise, WsProvider } from '@polkadot/api';
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
-async function main () {
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-    const api = await ApiPromise.create({ provider: wsProvider });
 
-    const polkadexParaId = 2040;
-    const polkadexGenesisBlock = 9743944;
-    const polkadexLastBlock = 10881400;
+const NETWORK_NAME = 'polkadot';
+const SUBSCAN_API_ENDPOINT = `https://${NETWORK_NAME}.api.subscan.io`;
+const SUBSCAN_API_KEY = '80cf9967908e942ff4f7acb43165595c';
+const POLKADEX_FUND_ACCOUNT = '13UVJyLnbVp77Z2t6qjFmcyzTXYQJjyb6Hww7ZHPumd81iht';
 
-    const selectedParaId = polkadexParaId;
-    const genesisBlock = polkadexLastBlock;
-    const lastBlock = polkadexLastBlock;
 
-    let data = {
-        'auction': 'Polkadex',
-        'wallets': {},
-        'total_transactions': 0,
-        'total_contributed': 0
-    };
-    let currentBlock = genesisBlock;
+function scrapeCrowdloanData () {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const CROWDLOAN_DATA_PATH = path.join(__dirname, 'crowdloan_data.json');
+    const LAST_BLOCK_NUMBER_PATH = path.join(__dirname, 'last_block_number.json');
+    const TRANSFERS_ENDPOINT = '/api/scan/transfers';
+    // const API_ENDPOINT = `${SUBSCAN_API_ENDPOINT}${TRANSFERS_ENDPOINT}`;
+    const POLKADEX_PARA_ID = 2040;
+    const POLKADEX_GENESIS_BLOCK_NUMBER = 9743944;
+    const POLKADEX_LAST_BLOCK_NUMBER = 10881400;
+    // const rowsPerPage = 100;
+    const rowsPerPage = 1;
 
-    // while (currentBlock <= api.rpc.chain.getHeader((header) => header.number)) {
-    while (currentBlock <= lastBlock) {
-        const blockHash = await api.rpc.chain.getBlockHash(currentBlock);
-        const signedBlock = await api.rpc.chain.getBlock(blockHash);
-        const allRecords = await api.query.system.events.at(signedBlock.block.header.hash);
+    let data = {};
+    let currentPage = 0;
+    let totalPages = 0;
+    let lastBlockNumber = POLKADEX_GENESIS_BLOCK_NUMBER;
 
-        allRecords
-        .forEach(({ event }) => {
-            if (event.section == 'crowdloan') {
-                let [ sender, paraId, amount ] = event.data.toJSON();
-                if (paraId === selectedParaId) {
-                    amount = amount / 10 ** 10;
-                    data['total_transactions'] += 1;
-                    data['total_contributed'] += amount;
-
-                    if (Object.keys(data.wallets).includes(sender)) {
-                        data.wallets[sender]['amount'] += amount;
-                        data.wallets[sender]['total_contributions'] += 1;
-                        data.wallets[sender]['blocks'].push({
-                            'block_hash': signedBlock.block.header.hash,
-                            'block_number': currentBlock
-                        })
-
-                        console.log(`${data['total_transactions']}. Actualizando contribución de (${sender}) en el bloque #${currentBlock}:`);
-                        console.log(`Sender Total Amount: ${data.wallets[sender]['amount']} DOTs`);
-                        console.log(`Auction Total Amount: ${data['total_contributed']} DOTs`);
-                    } else {
-                        data.wallets[sender] = {
-                            'para_id': paraId,
-                            'amount': amount,
-                            'total_contributions': 1,
-                            'blocks': [
-                                {
-                                    'block_hash': signedBlock.block.header.hash,
-                                    'block_number': currentBlock
-                                }
-                            ]
-                        }
-                        console.log(`${data['total_transactions']}. Nueva contribución en el bloque #${currentBlock}:`);
-                        console.log(`Sender (${sender}) - Amount (${amount} DOTs)`);
-                        console.log(`Auction Total Amount: ${data['total_contributed']} DOTs`);
-                    }
-                }
-            }
-        });
-
-        currentBlock += 1;
+    // Load JSON data if exists
+    if (checkExistingJSON(CROWDLOAN_DATA_PATH)) {
+        data = loadJSON(CROWDLOAN_DATA_PATH);
+    } else {
+        data = {
+            'auction': 'Polkadex',
+            'wallets': {},
+            'total_transactions': 0,
+            'total_contributed': 0
+        };
     }
+
+    const subscanApi = axios.create({
+        baseURL: SUBSCAN_API_ENDPOINT,
+        headers: {
+            'X-API-Key': SUBSCAN_API_KEY,
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (checkExistingJSON(LAST_BLOCK_NUMBER_PATH)) {
+        lastBlockNumber = loadJSON(LAST_BLOCK_NUMBER_PATH).lastBlockNumber;
+        console.log(`Loaded: ${lastBlockNumber}`);
+    }
+
+    subscanApi.post(TRANSFERS_ENDPOINT, {
+            address: POLKADEX_FUND_ACCOUNT,
+            row: rowsPerPage,
+            page: 0,
+            from_block: lastBlockNumber,
+            to_block: POLKADEX_LAST_BLOCK_NUMBER
+        })
+        .then((response) => {
+            totalPages = response.data.data.count / rowsPerPage;
+            const transfers = response.data.data.transfers;
+
+            for (const transfer of transfers) {
+                const sender = transfer.from;
+                const amount = parseFloat(transfer.amount);
+
+                data['total_transactions'] += 1;
+                data['total_contributed'] += amount;
+
+                if (Object.keys(data.wallets).includes(sender)) {
+                    data.wallets[sender]['amount'] += amount;
+                    data.wallets[sender]['total_contributions'] += 1;
+                    data.wallets[sender]['blocks'].push({
+                        'block_hash': transfer.hash,
+                        'block_number': transfer.block_num
+                    })
+
+                    console.log(`${data['total_transactions']}. Actualizando contribución de (${sender}) en el bloque #${transfer.block_num}:`);
+                    console.log(`Sender Total Amount: ${data.wallets[sender]['amount']} DOTs`);
+                    console.log(`Auction Total Amount: ${data['total_contributed']} DOTs`);
+                } else {
+                    data.wallets[sender] = {
+                        'para_id': POLKADEX_PARA_ID,
+                        'amount': amount,
+                        'total_contributions': 1,
+                        'blocks': [{
+                            'block_hash': transfer.hash,
+                            'block_number': transfer.block_num
+                        }]
+                    }
+                    console.log(`${data['total_transactions']}. Nueva contribución en el bloque #${transfer.block_num}:`);
+                    console.log(`Sender (${sender}) - Amount (${amount} DOTs)`);
+                    console.log(`Auction Total Amount: ${data['total_contributed']} DOTs`);
+                }
+
+                lastBlockNumber = transfer.block_num;
+                writeJSON(CROWDLOAN_DATA_PATH, data);
+
+                // verificar que el bloquie desde el cua lse empieza ya existe
+                // para no almacenarlo
+            }
+        })
+        .catch((err) => {
+            console.log('Identified error');
+            console.log(err);
+            console.log(`Current page: ${currentPage}`);
+        })
+        .finally(() => {
+            writeJSON(LAST_BLOCK_NUMBER_PATH, {
+                lastBlockNumber: lastBlockNumber
+            });
+        });
 }
 
-main()
+
+function loadJSON(path) {
+    const rawData = fs.readFileSync(path);
+    const data = JSON.parse(rawData);
+    return data;
+}
+
+
+function writeJSON(path, data) {
+    const parsedData = JSON.stringify(data);
+    fs.writeFileSync(path, parsedData);
+}
+
+
+function checkExistingJSON(path) {
+    return fs.existsSync(path);
+}
+
+scrapeCrowdloanData();
